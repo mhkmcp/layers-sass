8dc5577b9eeb2032e482064d0491a62d38d12aec
Humayun Kabir <mhkmcp@yahoo.com>
2024-11-18 23:23:43 +0100

    added Logo Ticker

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
                 features with an intuitive interface that in your creative flow.
             </p>
             <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
-                <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1" />
+                <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1 w-full" />
                 <Button type="submit" variant="primary" size="sm" className="whitespace-nowrap">Sign Up</Button>
             </form>
         </div>
